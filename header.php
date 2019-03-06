9d33f0e285556a0c17ccdc2d9a6aaa6a152381dd
nandoahmad <48055931+nandoahmad@users.noreply.github.com>
2019-03-06 19:24:20 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,55 @@
+	<!DOCTYPE html>
+	<html lang="zxx" class="no-js">
+	<head>
+		<!-- Mobile Specific Meta -->
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Favicon-->
+		<link rel="shortcut icon" href="img/fav.png">
+		<!-- Author Meta -->
+		<meta name="author" content="codepixer">
+		<!-- Meta Description -->
+		<meta name="description" content="">
+		<!-- Meta Keyword -->
+		<meta name="keywords" content="">
+		<!-- meta character set -->
+		<meta charset="UTF-8">
+		<!-- Site Title -->
+		<title>Blajar</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:100,200,400,300,500,600,700" rel="stylesheet"> 
+			<!--
+			CSS
+			============================================= -->
+			<link rel="stylesheet" href="assets/css/linearicons.css">
+			<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+			<link rel="stylesheet" href="assets/css/bootstrap.css">
+			<link rel="stylesheet" href="assets/css/magnific-popup.css">
+			<link rel="stylesheet" href="assets/css/nice-select.css">					
+			<link rel="stylesheet" href="assets/css/animate.min.css">
+			<link rel="stylesheet" href="assets/css/owl.carousel.css">
+			<link rel="stylesheet" href="assets/css/main.css">
+		</head>
+		<body>
+			
+			  <header id="header" id="home">
+			    <div class="container">
+			    	<div class="row align-items-center justify-content-between d-flex">
+				      <div class="navbar-header">
+			                </button>
+			                <a class="navbar-brand" href="index.php">B-Lajar</a>
+			            </div>
+				      <nav id="nav-menu-container">
+				        <ul class="nav-menu">
+				          <li class="menu-active"><a href="#home">Home</a></li>
+				          <li><a href="#about">About Us</a></li>
+				          <li><a href="#goal">We Offfer</a></li>
+				          <li><a href="#contact">Contact</a></li>
+				          <li><a href="login/login_siswa.php">Sign In</a></li>
+				          </li>
+				        </ul>
+				      </nav><!-- #nav-menu-container -->		    		
+			    	</div>
+			    </div>
+			  </header><!-- #header -->
+</body>
+</html>
